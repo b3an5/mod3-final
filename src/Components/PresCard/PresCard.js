e2c2145a72f42ee0cdc15a9e4d5609449ea8ed13
B3an5 <beantheo@gmail.com>
2019-06-03 11:52:44 -0600

    cleanup

--- src/Components/PresCard/PresCard.js
+++ src/Components/PresCard/PresCard.js
@@ -2,19 +2,32 @@ import React from "react";
 import "./PresCard.css";
 
 export default function PresCard(props) {
+  let {
+    number,
+    president,
+    birth_year,
+    death_year,
+    took_office,
+    left_office,
+    party
+  } = props.presidentInfo;
+  if (death_year === null) {
+    death_year = "now";
+  }
+  if (left_office === null) {
+    left_office = "now";
+  }
   return (
     <div className="card">
-      <h2>{props.presidentInfo.number}</h2>
-      <h3>{props.presidentInfo.president}</h3>
+      <h2>{number}</h2>
+      <h3>{president}</h3>
       <h3>
-        alive:{props.presidentInfo.birth_year} to{" "}
-        {props.presidentInfo.death_year}
+        alive:{birth_year} to {death_year}
       </h3>
       <h4>
-        president from:{props.presidentInfo.took_office} to{" "}
-        {props.presidentInfo.left_office}
+        president from:{took_office} to {left_office}
       </h4>
-      <h3>{props.presidentInfo.party}</h3>
+      <h3>{party}</h3>
     </div>
   );
 }
